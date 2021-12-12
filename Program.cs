13e603fb742d1f9b5dbdeace384e22369d9a1f7e
Celal Ergun <celalergun@gmail.com>
2021-12-12 23:09:33 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace douzyuzdohsansekiz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sonuc = Bol(1, 998001);
+            Console.WriteLine(sonuc);
+        }
+
+        static string Bol(int kaci, int kaca)
+        {
+            var sonuc = "";
+            int sayi = kaci;
+
+            while (sayi > 0)
+            {
+                while (sayi < kaca)
+                {
+                    sayi = sayi * 10;
+                    if (sayi < kaca)
+                        sonuc = sonuc + "0";
+                }
+                int bolum = sayi / kaca;
+                int kalan = sayi % kaca;
+                sayi = kalan;
+                sonuc = sonuc + bolum;
+                if (sonuc.EndsWith("999"))
+                    break;
+            }
+            return sonuc;
+        }
+    }
+}
