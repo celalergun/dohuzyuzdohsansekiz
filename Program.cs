af41ea07b0398144f5260cdcd7764897f06355a6
Celal Ergun <celalergun@gmail.com>
2021-12-17 15:52:22 +0300

    Gruplama

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace douzyuzdohsansekiz
 {
@@ -6,6 +8,7 @@ namespace douzyuzdohsansekiz
     {
         static void Main(string[] args)
         {
+            //string sonuc = Bol(1, 998001);
             string sonuc = Bol(1, 998001);
             Console.WriteLine(sonuc);
         }
@@ -13,6 +16,7 @@ namespace douzyuzdohsansekiz
         static string Bol(int kaci, int kaca)
         {
             var sonuc = "";
+
             int sayi = kaci;
 
             while (sayi > 0)
@@ -27,10 +31,22 @@ namespace douzyuzdohsansekiz
                 int kalan = sayi % kaca;
                 sayi = kalan;
                 sonuc = sonuc + bolum;
-                if (sonuc.EndsWith("999"))
+                if (sonuc.Length >= 2997)
                     break;
             }
-            return sonuc;
+
+            List<string> a = new List<string>();
+            if (kaci < kaca)
+                a.Add("0.");
+
+            for (int i = 0; i < sonuc.Length; i += 3)
+            {
+                if ((i + 3) < sonuc.Length)
+                    a.Add(sonuc.Substring(i, 3));
+                else
+                    a.Add(sonuc.Substring(i));
+            }
+            return string.Join(" ", a);
         }
     }
 }
